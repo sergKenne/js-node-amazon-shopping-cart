ee72b5a31ec1a34f6f0d98084afc05010a0051e3
Kenne Serge <kenne_serge@mail.ru>
2020-08-10 11:32:51 +0300

    build the url router

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,11 +6,11 @@ const HomeScreen = {
         return `<ul class="products">
             ${products.map( product => {
                 return `<li class="product products__item">
-                        <a href="#" class="product__image">
+                        <a href="/#/product/${product._id}" class="product__image">
                             <img src="${product.image}" alt="${product.name}" class="product__picture">
                         </a>
                         <div class="product__body">
-                            <a href="" class="product__name">${product.name}</a>
+                            <a href="/#/product/${product._id}" class="product__name">${product.name}</a>
                             <div class="product__brand">${product.brand}</div>
                             <div class="product__price">$${product.price}</div>
                         </div>
